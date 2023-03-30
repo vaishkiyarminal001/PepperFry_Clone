f94b1bc9b8efd6beebb84d3f867b74c827813b02
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2023-03-30 09:41:08 +0530

    day-2 push

---
+++ scripts/home.js
@@ -0,0 +1,47 @@
+// scroll
+
+// var style = document.createElement('style');
+// var position = 'right';
+// style.innerHTML = `@keyframes my-animation{
+//     0%{${position:-${document.querySelector("#caraousel").offsetWidth + 10}px;}
+
+// }`;
+
+
+
+// timer
+var timer = document.getElementById("timer");
+
+var endTime = new Date(Date.now() + 11 * 60 * 60 * 1000 + 46 * 60 * 1000 + 35 * 1000);
+
+// update the timer every second
+var timerInterval = setInterval(updateTimer, 1000);
+
+function updateTimer() {
+  // calculate the time remaining
+  var timeRemaining = endTime - Date.now();
+
+  // if the timer has ended, clear the interval and display "Timer Ended" text
+  if (timeRemaining <= 0) {
+    clearInterval(timerInterval);
+    timer.textContent = "Timer Ended";
+    return;
+  }
+
+  // format the time remaining as hours, minutes, and seconds
+  var hours = Math.floor(timeRemaining / (60 * 60 * 1000));
+  var minutes = Math.floor((timeRemaining % (60 * 60 * 1000)) / (60 * 1000));
+  var seconds = Math.floor((timeRemaining % (60 * 1000)) / 1000);
+
+  // display the formatted time remaining
+  timer.textContent =  `Ending in ${hours}h : ${minutes}m : ${seconds}s`;
+}
+
+
+// var heartIcon = document.getElementById("heartIcon");
+
+// heartIcon.addEventListener("click", ()=>{
+
+// })
+
+
